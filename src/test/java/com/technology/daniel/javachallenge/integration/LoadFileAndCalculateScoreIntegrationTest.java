c95c6866c58b1e227a6409e0ba9038abdaaa2644
Daniel Fucci <daniel.m.fucci@gmail.com>
2021-05-31 19:05:06 -0300

    Adding integration test

---
+++ src/test/java/com/technology/daniel/javachallenge/integration/LoadFileAndCalculateScoreIntegrationTest.java
@@ -0,0 +1,72 @@
+package com.technology.daniel.javachallenge.integration;
+
+import com.technology.daniel.javachallenge.domain.model.Frame;
+import com.technology.daniel.javachallenge.service.FileParser;
+import com.technology.daniel.javachallenge.service.ScoreService;
+import com.technology.daniel.javachallenge.service.implementation.FileParserImpl;
+import com.technology.daniel.javachallenge.service.implementation.ScoreServiceImpl;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LoadFileAndCalculateScoreIntegrationTest {
+    @Test
+    public void LoadFileAndCalculatePerfectScoreTest() {
+        FileParser fileParser = new FileParserImpl();
+        ScoreService scoreService = new ScoreServiceImpl();
+
+        List<Frame> frames = fileParser.loadMatchFramesFromFile("src/test/resources/positive/perfect.txt");
+
+        Map<String, List<Frame>> framesByPlayer = scoreService.divideMatchFramesPerPlayer(frames);
+
+        scoreService.generateScore(framesByPlayer);
+
+        assertEquals(framesByPlayer.get("Carl").get(9).getScore() == 300, true);
+    }
+
+    @Test
+    public void LoadFileAndCalculateAllZeroScoreTest() {
+        FileParser fileParser = new FileParserImpl();
+        ScoreService scoreService = new ScoreServiceImpl();
+
+        List<Frame> frames = fileParser.loadMatchFramesFromFile("src/test/resources/positive/all-rows-zero.txt");
+
+        Map<String, List<Frame>> framesByPlayer = scoreService.divideMatchFramesPerPlayer(frames);
+
+        scoreService.generateScore(framesByPlayer);
+
+        assertEquals(framesByPlayer.get("Carl").get(9).getScore() == 0, true);
+    }
+
+    @Test
+    public void LoadFileAndCalculateAllFaultScoreTest() {
+        FileParser fileParser = new FileParserImpl();
+        ScoreService scoreService = new ScoreServiceImpl();
+
+        List<Frame> frames = fileParser.loadMatchFramesFromFile("src/test/resources/positive/all-rows-F.txt");
+
+        Map<String, List<Frame>> framesByPlayer = scoreService.divideMatchFramesPerPlayer(frames);
+
+        scoreService.generateScore(framesByPlayer);
+
+        assertEquals(framesByPlayer.get("Carl").get(9).getScore() == 0, true);
+    }
+
+    @Test
+    public void LoadFileAndCalculateOrdinaryScoreTest() {
+        FileParser fileParser = new FileParserImpl();
+        ScoreService scoreService = new ScoreServiceImpl();
+
+        List<Frame> frames = fileParser.loadMatchFramesFromFile("src/test/resources/positive/scores.txt");
+
+        Map<String, List<Frame>> framesByPlayer = scoreService.divideMatchFramesPerPlayer(frames);
+
+        scoreService.generateScore(framesByPlayer);
+
+        assertEquals(framesByPlayer.get("John").get(9).getScore() == 151 && framesByPlayer.get("Jeff").get(9).getScore() == 167, true);
+    }
+
+}
